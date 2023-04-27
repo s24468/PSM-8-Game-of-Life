57668d8615c850b5bb83f20e1598b7c37d442562
s24468 <s24468@pjwstk.edu.pl>
2023-04-27 18:03:58 +0200

    Algotihms UpdateCells method commit and WindowApp update

---
+++ PSM_8/Algorithms.cs
@@ -0,0 +1,54 @@
+﻿using System.Windows.Shapes;
+namespace PSM_8
+{
+    public class Algorithms
+    {
+        private int Size { get; }
+
+        public Algorithms(int size)
+        {
+            Size = size;
+        }
+
+        public void UpdateCells(bool[,] nextGeneration, Rectangle[,] cells)
+        {
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    if (nextGeneration[i, j])
+                    {
+                        cells[i, j].Fill = System.Windows.Media.Brushes.Black;
+                    }
+                    else
+                    {
+                        cells[i, j].Fill = System.Windows.Media.Brushes.White;
+                    }
+                }
+            }
+        }
+        public int GetNumberOfAliveNeighbors(bool[,] _currentGeneration,int xCoordinate, int yCoordinate)
+        {
+            int aliveNeighbors = 0;
+            for (var x = -1; x <= 1; x++)
+            {
+                for (var y = -1; y <= 1; y++)
+                {
+                    if (x == 0 && y == 0)
+                    {
+                        continue;
+                    }
+
+                    int ii = xCoordinate + x;
+                    int jj = yCoordinate + y;
+                    if (ii >= 0 && ii < Size && jj >= 0 && jj < Size && _currentGeneration[ii, jj])// ZWYKŁA TABLICA
+                    {
+                        aliveNeighbors++;
+                    }
+                }
+            }
+
+            return aliveNeighbors;
+        }
+    }
+}
--- PSM_8/MainWindow.xaml.cs
+++ PSM_8/MainWindow.xaml.cs
@@ -1,28 +1,128 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using System.Windows.Threading;
+using System.Windows.Shapes;
 namespace PSM_8
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private const int Size = 20;
+        private readonly Rectangle[,] _cells = new Rectangle[Size, Size];
+        private readonly DispatcherTimer _timer = new DispatcherTimer();
+        private bool[,] _currentGeneration = new bool[Size, Size];
+        private bool[,] _nextGeneration = new bool[Size, Size];
+        private Algorithms _algorithms = new Algorithms(Size);
+
         public MainWindow()
         {
             InitializeComponent();
+
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    Rectangle rect = new Rectangle
+                    {
+                        Width = 20,
+                        Height = 20,
+                        Fill = System.Windows.Media.Brushes.White,
+                        Stroke = System.Windows.Media.Brushes.Black,
+                        StrokeThickness = 1
+                    };
+                    Canvas.SetLeft(rect, i * 20);
+                    Canvas.SetTop(rect, j * 20);
+                    canvas.Children.Add(rect);
+                    _cells[i, j] = rect;
+                }
+            }
+
+            _currentGeneration[2, 1] = true;
+            _currentGeneration[2, 2] = true;
+            _currentGeneration[2, 3] = true;
+            _currentGeneration[1, 2] = true;
+            _currentGeneration[0, 1] = true;
+
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    if (_currentGeneration[i, j])
+                    {
+                        _cells[i, j].Fill = System.Windows.Media.Brushes.Black;
+                    }
+                }
+            }
+
+            _timer.Interval = TimeSpan.FromMilliseconds(200);
+            _timer.Tick += Timer_Tick;
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            _timer.Start();
+        }
+
+        private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            _timer.Stop();
+        }
+
+        private void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+            _timer.Stop();
+            _currentGeneration = new bool[Size, Size];
+            _nextGeneration = new bool[Size, Size];
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    _cells[i, j].Fill = System.Windows.Media.Brushes.White;
+                }
+            }
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            // Calculate the next generation based on the current generation
+            CalculateNextGeneration();
+            
+
+            // Update the cells on the canvas
+            _algorithms.UpdateCells(_nextGeneration, _cells);
+            // Update the current generation with the next generation
+            _currentGeneration = (bool[,])_nextGeneration.Clone();
+        }
+
+        private void CalculateNextGeneration()
+        {
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    int aliveNeighbors =
+                        _algorithms.GetNumberOfAliveNeighbors(_currentGeneration, i, j); //GetAliveNeighbors(i, j);
+                    if (_currentGeneration[i, j])
+                    {
+                        if (aliveNeighbors < 2 || aliveNeighbors > 3)
+                        {
+                            _nextGeneration[i, j] = false;
+                        }
+                        else
+                        {
+                            _nextGeneration[i, j] = true;
+                        }
+                    }
+                    else
+                    {
+                        if (aliveNeighbors == 3)
+                        {
+                            _nextGeneration[i, j] = true;
+                        }
+                    }
+                }
+            }
         }
     }
 }
